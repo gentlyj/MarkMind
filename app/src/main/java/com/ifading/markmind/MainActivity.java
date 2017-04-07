836bfa31c38d6e92ca599dcad8fa97bb2f366535
yangjingsheng <826821901@qq.com>
2017-04-07 21:00:49 +0800

    [code] add shortcut bar

--- app/src/main/java/com/ifading/markmind/MainActivity.java
+++ app/src/main/java/com/ifading/markmind/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.Selection;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +29,11 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final byte SHORTCUT_BOLD = 1;
+    private static final byte SHORTCUT_TITLE = 2;
+    private static final byte SHORTCUT_LIST = 3;
+    private static final byte SHORTCUT_CENTER = 4;
+    private static final byte SHORTCUT_QUOTE = 5;
     @BindView(R.id.parentLayout)
     protected RelativeLayout mParentLayout;
     @BindView(R.id.markDown_layout)
@@ -39,6 +47,17 @@ public class MainActivity extends AppCompatActivity {
     //软件键盘是否显示
     private boolean mSoftKeyboardDisplay;
 
+
+    @BindView(R.id.edit_page_short_cut_title)
+    protected TextView mShortCutTitle;
+    @BindView(R.id.edit_page_short_cut_center)
+    protected TextView mShortCutCenter;
+    @BindView(R.id.edit_page_short_cut_list)
+    protected TextView mShortCutList;
+    @BindView(R.id.edit_page_short_cut_bold)
+    protected TextView mShortCutBold;
+    @BindView(R.id.edit_page_short_cut_quote)
+    protected TextView mShortCutQuote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,13 +99,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "keyboard height = " + realKeyboardHeight);
 
 
-                if (realKeyboardHeight>100) {
-                    if (!mSoftKeyboardDisplay){
+                if (realKeyboardHeight > 100) {
+                    if (!mSoftKeyboardDisplay) {
                         onSoftKeyboardDisplay(realKeyboardHeight);
                     }
                     mSoftKeyboardDisplay = true;
-                }else{
-                    if (mSoftKeyboardDisplay){
+                } else {
+                    if (mSoftKeyboardDisplay) {
                         onSoftKeyboardHidden();
                     }
                     mSoftKeyboardDisplay = false;
@@ -115,14 +134,138 @@ public class MainActivity extends AppCompatActivity {
         mMarkDownLayout.setVisibility(View.VISIBLE);
     }
 
-    @OnClick(R.id.button)
-    protected void onClick(){
-        showToast("点击按钮了");
-        // 设置为Markdown
-        RichText.fromMarkdown(mEd.getEditableText().toString()).into(mMDTv);
+    @OnClick({R.id.button, R.id.edit_page_short_cut_bold
+            , R.id.edit_page_short_cut_center
+            , R.id.edit_page_short_cut_quote
+            , R.id.edit_page_short_cut_list
+            , R.id.edit_page_short_cut_title})
+    protected void onClick(View v) {
+        if (v == mButton) {
+            showToast("点击按钮了");
+            // 设置为Markdown
+            RichText.fromMarkdown(mEd.getEditableText().toString()).into(mMDTv);
+        } else if (v == mShortCutBold) {
+            showToast("点击Bold了");
+            clickShotrCut(SHORTCUT_BOLD);
+        } else if (v == mShortCutTitle) {
+            //showToast("点击Title了");
+            clickShotrCut(SHORTCUT_TITLE);
+            /*int currentCursorLine = getCurrentCursorLine(mEd);
+            String multiLines = mEd.getText().toString();
+            String[] streets;
+            String delimiter = "\n";
+
+            streets = multiLines.split(delimiter);
+            String currentLine = streets[currentCursorLine];
+            showToast(currentLine);
+
+            int startPos = mEd.getLayout().getLineStart(currentCursorLine);
+            int endPos = mEd.getLayout().getLineEnd(currentCursorLine);
+            Log.d(TAG, "当前行,startPos:" + startPos + " endPos:" + endPos + " currentLine:" + currentLine);
+
+            String substringFirst = mEd.getText().toString().substring(0, startPos);
+            String substringEnd = mEd.getText().toString().substring(endPos);
+
+            Log.d(TAG, "当前行,substringFirst:" + substringFirst + " substringEnd:" + substringEnd);
+            String add;
+            if (currentLine.startsWith("#")) {
+                add = "#" + currentLine;
+            } else {
+                add = "# " + currentLine;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(substringFirst).append(add).append(substringEnd);
+
+            mEd.setText(sb.toString());*/
+
+
+        } else if (v == mShortCutList) {
+            showToast("点击List了");
+            clickShotrCut(SHORTCUT_LIST);
+        } else if (v == mShortCutCenter) {
+            showToast("点击Center了");
+            clickShotrCut(SHORTCUT_CENTER);
+        } else if (v == mShortCutQuote) {
+            showToast("点击Quote了");
+            clickShotrCut(SHORTCUT_QUOTE);
+        }
+    }
+
+
+    private void clickShotrCut(byte type) {
+
+        int currentCursorLine = getCurrentCursorLine(mEd);
+        String multiLines = mEd.getText().toString();
+        String[] streets;
+        String delimiter = "\n";
+
+        streets = multiLines.split(delimiter);
+        String currentLine = streets[currentCursorLine];
+        Log.d(TAG, "当前行, currentLine:" + currentLine);
+        int startPos = mEd.getLayout().getLineStart(currentCursorLine);
+        int endPos = mEd.getLayout().getLineEnd(currentCursorLine);
+        String substringFirst = mEd.getText().toString().substring(0, startPos);
+        String substringEnd = mEd.getText().toString().substring(endPos);
+        String add = currentLine;
+        switch (type) {
+            case SHORTCUT_TITLE:
+
+                if (currentLine.startsWith("# ")||currentLine.startsWith("## ")) {
+                    add = "#" + currentLine;
+                } else if (currentLine.startsWith("### ")){
+                    add = currentLine.replace("###","#");
+                }else {
+                    add = "# " + currentLine;
+                }
+                break;
+
+            case SHORTCUT_CENTER:
+                add = "[" + currentLine + "]";
+                break;
+
+            case SHORTCUT_LIST:
+                if (currentLine.startsWith("-")){
+                    break;
+                }
+                add = "- " + currentLine;
+                break;
+
+            case SHORTCUT_BOLD:
+
+                break;
+
+            case SHORTCUT_QUOTE:
+                if (currentLine.startsWith(">")){
+                    break;
+                }
+                add = "> " + currentLine;
+                break;
+
+            default:
+                add = currentLine;
+                break;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(substringFirst).append(add).append(substringEnd);
+
+        mEd.setText(sb.toString());
+        // TODO: 17/4/7 有bug,会吃掉换行
+        mEd.setSelection(startPos+add.length());
+    }
+
+    public int getCurrentCursorLine(EditText editText) {
+        int selectionStart = Selection.getSelectionStart(editText.getText());
+        Layout layout = editText.getLayout();
+
+        if (selectionStart != -1) {
+            return layout.getLineForOffset(selectionStart);
+        }
+
+        return -1;
     }
 
     private void showToast(String content) {
-        Toast.makeText(this.getApplicationContext(),content,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this.getApplicationContext(), content, Toast.LENGTH_SHORT).show();
     }
 }
